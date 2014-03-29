390504e85f034cd3bd070f5a09b124586e585446
Azadi Bogolubov <azadibogolubov@gmail.com>
2014-03-29 12:19:05 -0700

    Updated Android Side
    Updated UI
    Added Driver.java
    Relocated strings to strings.xml
    Did some commenting

--- InterestReducerAndroid/src/com/blogspot/TutorAzadi/InterestReducerAndroid/MainActivity.java
+++ InterestReducerAndroid/src/com/blogspot/TutorAzadi/InterestReducerAndroid/MainActivity.java
@@ -1,31 +1,30 @@
 package com.blogspot.TutorAzadi.InterestReducerAndroid;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.ArrayAdapter;
+import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Spinner;
 import android.widget.Toast;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends Activity {
 
-	private Spinner durationSpinner;
-	private Button simpleBtn, compoundBtn, monthlyBtn;
-	
+	private Button calculateBtn, amortizeBtn;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		initializeSpinner();		
-		initializeButtons();
+		getActionBar().setTitle("Obtain Loan Information");
+		if (savedInstanceState == null) {
+			getFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
 	}
 
 	@Override
@@ -48,44 +47,40 @@ public class MainActivity extends Activity {
 		return super.onOptionsItemSelected(item);
 	}
 
-	public void initializeSpinner()
-	{
-		List<String> items = new ArrayList<String>();
-		items.add("Month(s)");
-		items.add("Year(s)");
-		durationSpinner = (Spinner) findViewById(R.id.duration_spinner);
-		ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, items);
-		durationSpinner.setAdapter(spinnerAdapter);
-	}
-	
-	public void initializeButtons()
-	{
-		simpleBtn = (Button) findViewById(R.id.simpleBtn);
-		simpleBtn.setOnClickListener(new OnClickListener() {
+	public void initializeButtons() {
+		calculateBtn = (Button) findViewById(R.id.calculateBtn);
+		calculateBtn.setOnClickListener(new OnClickListener() {
 			@Override
-			public void onClick(View v)
-			{
-				Toast.makeText(MainActivity.this, "Simple Interest Request", Toast.LENGTH_LONG).show();
-			}
-		});
-		
-		
-		compoundBtn = (Button) findViewById(R.id.compoundBtn);
-		compoundBtn.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v)
-			{
-				Toast.makeText(MainActivity.this, "Compound Interest Request", Toast.LENGTH_LONG).show();
+			public void onClick(View v) {
+				Toast.makeText(MainActivity.this, "Calculate Payment Request",
+						Toast.LENGTH_LONG).show();
 			}
 		});
 
-		monthlyBtn = (Button) findViewById(R.id.monthlyBtn);
-		monthlyBtn.setOnClickListener(new OnClickListener() {
+		amortizeBtn = (Button) findViewById(R.id.amortizeBtn);
+		amortizeBtn.setOnClickListener(new OnClickListener() {
 			@Override
-			public void onClick(View v)
-			{
-				Toast.makeText(MainActivity.this, "Monthly Payment Request", Toast.LENGTH_LONG).show();
+			public void onClick(View v) {
+				Toast.makeText(MainActivity.this, "Amortization Request",
+						Toast.LENGTH_LONG).show();
 			}
 		});
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container,
+					false);
+			return rootView;
+		}
 	}
 }
