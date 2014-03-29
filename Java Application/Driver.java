163688f8d550b1f6a82854b36cbab629c56e07a4
Azadi Bogolubov <azadibogolubov@gmail.com>
2014-03-28 21:43:46 -0700

    Have amortization functionality in place.

--- Java Application/Driver.java
+++ Java Application/Driver.java
@@ -8,20 +8,29 @@ public class Driver
 {
 	public static double rate;
 	public static double principal;
-	public static int time;
+	public static double time;
+	public static double payment_amount;
 	public static double simple_interest;
 	public static double compound_interest;
+	public static double total_interest = 0.00f;
+	public static double principal_paid;
 	public static NumberFormat n = NumberFormat.getCurrencyInstance(Locale.US);
 	public static DecimalFormat df = new DecimalFormat("#.##");
 	
+	public static double amortize(double principal, double rate, double time)
+	{
+		rate /= 1200;
+		return principal * ((rate * Math.pow((1 + rate), time))/(Math.pow((1+rate),time)-1));
+	}
+
 	public static void main(String args[])
 	{
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter principal");
-		try { principal = scanner.nextDouble(); }
+		/*try { principal = scanner.nextDouble(); }
 		catch (InputMismatchException e) 
 		{ 
-			System.out.println("Invalid input"); 
+			System.out.println("Usage: Decimal or Integer input");
 			scanner.nextLine(); 
 		}
 		
@@ -29,7 +38,7 @@ public class Driver
 		try { time = scanner.nextInt(); }
 		catch (InputMismatchException e) 
 		{ 
-			System.out.println("Invalid input"); 
+			System.out.println("Usage: Decimal or Integer input");
 			scanner.nextLine(); 
 		}
 
@@ -37,10 +46,22 @@ public class Driver
 		try { rate = scanner.nextDouble(); }
 		catch (InputMismatchException e) 
 		{ 
-			System.out.println("Invalid input"); 
+			System.out.println("Usage: Decimal or Integer input");
 			scanner.nextLine(); 
 		}
+
+		System.out.println("Enter payment amount: (e.g. 500.50");
+		try { payment_amount = scanner.nextDouble(); }
+		catch (InputMismatchException e)
+		{
+			System.out.println("Usage: Decimal or Integer input");
+			scanner.nextLine();
+		}
 		scanner.close();
+		*/
+		principal = 100000;
+		rate = 5.25;
+		time = 360;
 		
 		System.out.println("Principal: " + n.format(principal));
 		System.out.println("Interest Rate: " + df.format(rate) + "%");
@@ -48,8 +69,18 @@ public class Driver
 		
 		simple_interest = principal * (rate / 100) * (time / 12);
 		System.out.println("Simple interest: " + n.format(simple_interest));
-
-		compound_interest = principal * Math.pow((1+ (rate / 100)),(time / 12));
-		System.out.println("Compound interest: " + n.format(compound_interest));
+		payment_amount = amortize(principal, rate, time);
+		for (int i = 0; i < time; i++)
+		{
+			System.out.println("Month #: " + i);
+			compound_interest = principal * (1+ (rate / 100) / 12) - principal;
+			total_interest += compound_interest;
+			System.out.println("Monthly payment amount is: " + n.format(payment_amount));
+			System.out.println("Interest charged = " + n.format(compound_interest));
+			principal_paid = payment_amount - compound_interest;
+			principal -= principal_paid;
+			System.out.println("Principal paid down = " + n.format(principal_paid) + "\n");
+		}
+		System.out.println("Total interest paid: " + n.format(total_interest));
 	}
 }
