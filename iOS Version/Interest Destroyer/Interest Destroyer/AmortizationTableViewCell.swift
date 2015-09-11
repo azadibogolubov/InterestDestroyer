5ea1e34d191525385523ce0c85a7a0756420ff25
Azadi Bogolubov <AzadiBogolubov@gmail.com>
2015-09-11 07:50:11 -0700

    Added amortization cell layout

---
+++ iOS Version/Interest Destroyer/Interest Destroyer/AmortizationTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  AmortizationTableViewCell.swift
+//  Interest Destroyer
+//
+//  Created by Azadi on 9/10/15.
+//  Copyright (c) 2015 Tutor Azadi. All rights reserved.
+//
+
+import UIKit
+
+class AmortizationTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var monthLbl: UILabel!
+    @IBOutlet weak var minimumPaymentLbl: UILabel!
+    @IBOutlet weak var extraPaymentLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
