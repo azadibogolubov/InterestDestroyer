c80ee03f0cb2b9c5386efbb21d0d1d7849c196d4
Azadi Bogolubov <AzadiBogolubov@gmail.com>
2016-02-23 22:08:54 -0800

    Added stub for Espresso

---
+++ Android Version/Interest Destroyer/app/src/androidTest/java/BasicEspressoTests.java
@@ -0,0 +1,26 @@
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import com.tutorazadi.interestdestroyerandroid.MainActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class BasicEspressoTests {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
+
+    @Test
+    public void listGoesOverTheFold() {
+        onView(withText("Hello world!")).check(matches(isDisplayed()));
+    }
+}
